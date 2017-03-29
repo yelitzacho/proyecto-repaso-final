9066da84ff0d33d7d969758f367a3b0885d599a3
Yelitza Cho <yelitza.choque@gmail.com>
2017-03-29 13:49:35 -0500

    Validando

--- registro.js
+++ registro.js
@@ -23,7 +23,7 @@ window.addEventListener('load', function(){
     ingresoButton.addEventListener('click', function(evento){
         evento.preventDefault();
         if (nombre.value.length != 0 && apellido.value.length != 0 && mail.value.length != 0 && contrasena.value.length != 0) {
-            var ingreso = new nuevoCoder(nombre.value, apellido.value, mail.value, contrasena.value);
+            var ingreso = new Coders(nombre.value, apellido.value, mail.value, contrasena.value);
                 localStorage.setItem("nueva-Coder",JSON.stringify(ingreso));
                 window.location = "inicia-sesion.html"
                 registro.innerHTML += coder[coder.length -1].registroCoders();
@@ -33,49 +33,54 @@ window.addEventListener('load', function(){
                 indicaciones.innerText = "Todos los campos deben ser llenados";
         }
     });
+
+    //Validacion de Letras
+    var letras = function(evento){
+        var codigos = evento.keyCode;
+            if ((codigos >= 65 && codigos <= 90) || (codigos >= 97 && codigos <= 122)){
+                return true;
+            } else{
+                return false;
+            }
+      }
+
+    nombre.onkeypress = letras;
+    apellido.onkeypress = letras;
+
+    //Validacion de Contraseña
+    var email = function(evento){
+        var codigos = evento.keyCode;
+        var longitud = this.value.length
+            if ((codigos >= 65 && codigos <= 90) || (codigos >= 97 && codigos <= 122) || (codigos >= 97 && codigos <= 122)){
+                return true;
+            } else{
+                return false;
+            }
+    }
+
+    mail.onkeypress = email;
+
+    //Validacion de Contraseña
+    var password = function(evento){
+        var codigos = evento.keyCode;
+        var longitud = this.value.length;
+            if ((codigos >= 48 && codigos <= 57) || (longitud >=6 && longitud <= 20)){
+                return true;
+            } else{
+                return false;
+            }
+    }
+
+    contrasena.onkeypress = password;
 });
 
-//Validacion de Letras
-var letras = function(evento){
-    var codigos = evento.keyCode;
-        if ((codigos >= 65 && codigos <= 90) || (codigos >= 97 && codigos <= 122) || codigos == 32){
-            return true;
-        } else{
-            return false;
-        }
-  }
-
-nombre.onkeypress = nombre;
-
-//Validacion de Contraseña
-var password = function(evento){
-    var codigos = evento.keyCode;
-    var longitud = this.value.length;
-        if ((codigos >= 48 && codigos <= 57) || (longitud >=6 && longitud <= 20)){
-            return true;
-        } else{
-            return false;
-        }
-}
-
-contrasena.onkeypress = password;
-
-var inputs = document.getElementsByClass('registro-coder');
+var inputs = document.getElementsByClassName('registro-coder');
 var validaciones = function(){
     if(this.value.trim().length == 0){
         this.value = "";
         this.nextElementSibling.nextElementSibling.innerText = "Este campo es obligatorio";
     }else{
         this.nextElementSibling.nextElementSibling.innerText = "";
-    }
-
-    if (this.getAttribute("type")=="text"){
-        var datos = this.value.split(" ");
-        var datosMayusculas = "";
-        for (var i = 0; i < datos.length; i++){
-            datosMayusculas += datos[i].charAt(0).toUpperCase() + datos[i].substring(1).toLowerCase() + " ";
-        }
-        this.value = datosMayusculas
     }
 }
 
